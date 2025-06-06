0d3d8fbbeac01595d778debe7a8574d0b828309f
Caro-elizabeth-quinan <ca.quinan@duocuc.cl>
2025-06-05 20:38:17 -0400

    primer commit -estructura del menu

---
+++ ejercicio.py
@@ -0,0 +1,26 @@
+#ejercicio sistema de pedidos 
+import os,time
+os.system("cls")
+pedidos=[]
+pizzas=[]
+while True:
+        print("=== PIZZERÍA LA MÁS RICA ===")
+        print("1. Registrar nueva pizza")
+        print("2. Ver catálogo de pizzas")
+        print("3. Realizar pedido")
+        print("4. Ver pedidos realizados")
+        print("5. Salir")
+        opcion=input("selecciones una opción (1-5):") 
+        if opcion=="1":
+                pass
+        elif opcion=="2":
+                pass
+        elif opcion=="3":
+                pass
+        elif opcion=="4":
+                pass
+        elif opcion=="5":
+                print("saliendo...")
+        else:
+                print("la opción no es correcta.intentalo de nuevo.")
+        input("presiona una tecla para continuar....")
